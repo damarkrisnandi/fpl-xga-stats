1d9331cc1bc1e11e4cd4505ab57835f7e7fe079f
damar <damar@gitlab.lab>
2023-09-06 06:55:06 +0700

    calculation

--- services/index.ts
+++ services/index.ts
@@ -72,26 +72,30 @@ export const leagueHistory = [
 
 export const getExpectedPoints = (element, gameWeek) => {
     let xP = 0;
-    const {element_type, bonus, expected_goals_per_90, expected_assists_per_90, starts_per_90, clean_sheets_per_90} = element;
+    const {element_type, bonus, expected_goals_per_90, expected_assists_per_90, starts_per_90, clean_sheets_per_90, own_goals, expected_goals_conceded_per_90} = element;
     if (element_type === 4) {
         const xPG = expected_goals_per_90 * 4;
         const xPA = expected_assists_per_90 * 3;
         const pMP = starts_per_90 >= 0.67 ? 2 : (starts_per_90 == 0 ? 0 : 1);
-        xP = xPG + xPA + pMP + (bonus/gameWeek); 
+        const xOG = (own_goals/gameWeek) * -1;
+        xP = xPG + xPA + pMP + (bonus/gameWeek) + xOG; 
     }
     if (element_type === 3) {
         const xPG = expected_goals_per_90 * 4;
         const xPA = expected_assists_per_90 * 3;
         const xCS = clean_sheets_per_90 * 1;
         const pMP = starts_per_90 >= 0.67 ? 2 : (starts_per_90 == 0 ? 0 : 1);
-        xP = xPG + xPA + xCS + pMP + (bonus/gameWeek); 
+        const xOG = (own_goals/gameWeek) * -1;
+        xP = xPG + xPA + xCS + pMP + (bonus/gameWeek) + xOG; 
     }
     if (element_type === 2) {
         const xPG = expected_goals_per_90 * 4;
         const xPA = expected_assists_per_90 * 3;
         const xCS = clean_sheets_per_90 * 4;
         const pMP = starts_per_90 >= 0.67 ? 2 : (starts_per_90 == 0 ? 0 : 1);
-        xP = xPG + xPA + xCS + pMP + (bonus/gameWeek); 
+        const xOG = (own_goals/gameWeek) * -1;
+        const xGC = expected_goals_conceded_per_90 >= 2 ? -1 : 0
+        xP = xPG + xPA + xCS + pMP + (bonus/gameWeek) + xOG + xGC; 
     }
 
     if (element_type === 1) {
@@ -99,7 +103,9 @@ export const getExpectedPoints = (element, gameWeek) => {
         const xPA = expected_assists_per_90 * 3;
         const xCS = clean_sheets_per_90 * 5;
         const pMP = starts_per_90 >= 0.67 ? 2 : (starts_per_90 == 0 ? 0 : 1);
-        xP = xPG + xPA + xCS + pMP + (bonus/gameWeek); 
+        const xOG = (own_goals/gameWeek) * -1;
+        const xGC = expected_goals_conceded_per_90 >= 2 ? -1 : 0
+        xP = xPG + xPA + xCS + pMP + (bonus/gameWeek) + xOG + xGC; 
     }
 
     return xP;
@@ -142,7 +148,7 @@ export const getTotalXPMultiplies = (bootstrap, gameWeek, picksData, fixtures) =
             if (home) {
                 const haIdx = haIndexData.find(ha => ha.home === dataEl.team);
                 if (attacks.includes(dataEl.element_type)) {
-                    haIdxValue = haIdx.homeOff / haIdx.awayDef;
+                    haIdxValue = haIdx.homeOff / haIdx.awayDef ;
                 } else {
                     haIdxValue = haIdx.homeDef / haIdx.awayOff;
                 }
