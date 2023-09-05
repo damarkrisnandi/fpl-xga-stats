ca0196de7384325913f95160af26eb8503ab2156
damar <damar@gitlab.lab>
2023-09-05 14:41:43 +0700

    add surplus

--- components/ElementCard.jsx
+++ components/ElementCard.jsx
@@ -194,8 +194,8 @@ export default function ElementCard(props) {
                     {
                         multiplier ? (
                             <div className="flex font-semibold">
-                                <p className={`mr-2 mb-2 tracking-tight text-gray-900  ${event_points - xP >= 0 ? 'text-green-700 dark:text-green-400' : 'text-red-700 dark:text-red-400'}`}>TxP {(xP * multiplier).toFixed(2)}</p>
-                                <p className={`mr-2 mb-2 tracking-tight text-gray-900  ${event_points - xP >= 0 ? 'text-green-700 dark:text-green-400' : 'text-red-700 dark:text-red-400'}`}>TP {event_points * multiplier}</p>
+                                <p className={`mr-2 mb-2 tracking-tight text-gray-900  ${event_points - xP >= 0 ? 'text-green-700 dark:text-green-400' : 'text-red-700 dark:text-red-400'}`}>&Sigma;xP {(xP * multiplier).toFixed(2)}</p>
+                                <p className={`mr-2 mb-2 tracking-tight text-gray-900  ${event_points - xP >= 0 ? 'text-green-700 dark:text-green-400' : 'text-red-700 dark:text-red-400'}`}>&Sigma;P {event_points * multiplier}</p>
                                 <p className={`mr-2 mb-2 tracking-tight text-gray-900  ${event_points - xP >= 0 ? 'text-green-700 dark:text-green-400' : 'text-red-700 dark:text-red-400'}`}> {`(${(event_points - xP) >= 0 ? '+' : ''}${((event_points - xP) * multiplier).toFixed(2)})`}</p>
                             </div>
                         ) : null
