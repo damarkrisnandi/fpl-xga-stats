be29a2eb466586a06c953c30fcf67dd01034deba
damar <damar@gitlab.lab>
2023-09-05 09:40:17 +0700

    fix props

--- app/mid/page.jsx
+++ app/mid/page.jsx
@@ -5,6 +5,10 @@ import Image from 'next/image';
 
 export default async function FilterByPosition() {
   const bootstrap = (await getBootstrap());
+  const teams = bootstrap.teams;
+  const gameWeek = bootstrap.events.find(o => o.is_current).id;
+  const nextGameWeek = bootstrap.events.find(o => o.is_next).id;
+
 
   let elements = bootstrap.elements;
   elements = elements.filter(o => o.element_type === 3)
@@ -15,6 +19,11 @@ export default async function FilterByPosition() {
         {elements.map(element => (
           <ElementCard
             { ...element }
+
+            
+            teams={teams}
+            gameWeek={gameWeek}
+            nextGameWeek={nextGameWeek}
           />
         ))}
       </div>
