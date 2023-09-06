168cf06fffb3fd96d338afeb722f076037956625
damar <damar@gitlab.lab>
2023-09-06 14:48:35 +0700

    add transition

--- components/AllTotalXP.jsx
+++ components/AllTotalXP.jsx
@@ -1,7 +1,21 @@
+'use client';
+import { useState, useEffect } from 'react'
+
 import { lowerBound } from '../services/index' 
 
 export default function TotalExpectedPtsNext(props) {
-    const { totalXPointsList } = props
+    const { totalXPointsList } = props;
+    const [width, setWidth] = useState(0);
+
+    useEffect((props) => {
+        if (!props) {
+            // for (let i=0; i<10; i++) {
+                setTimeout(() => {
+                    setWidth(1);
+                }, 1000);
+            // }
+        }
+    })
 
     const maxXp = Math.max(...totalXPointsList.map(xp => xp.totalXPoints))
     const minXp = Math.min(...totalXPointsList.map(xp => xp.totalXPoints))
@@ -14,7 +28,8 @@ export default function TotalExpectedPtsNext(props) {
                     <p className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Total xP Next Gameweek until GW38</p>
                     {totalXPointsList.map(xp => (
                         <div className="w-full mb-1 bg-gray-200 rounded-full dark:bg-gray-700">
-                            <div className={`${xp.totalXPoints > lowerBound ? (xp.totalXPoints === maxXp ? 'bg-fuchsia-800' : 'bg-blue-700') : (xp.totalXPoints === minXp ? 'bg-red-700' : 'bg-yellow-600')} text-xs font-medium text-blue-100 text-center p-0.5 rounded-full`} style={{width: `${((xp.totalXPoints / maxXp) * 100) - 5}%`}}>{ `GW${xp.gameWeek}: ${ (xp.totalXPoints).toFixed(2) } Pts`}</div>
+                            <div className={`${xp.totalXPoints > lowerBound ? (xp.totalXPoints === maxXp ? 'bg-fuchsia-800' : 'bg-blue-700') : (xp.totalXPoints === minXp ? 'bg-red-700' : 'bg-yellow-600')} text-xs font-medium text-blue-100 text-center p-0.5 rounded-full transition-all duration-700 
+                ease-out`} style={{width: `${(((xp.totalXPoints / maxXp) * 100) - 5)*width}%`}}>{ width > 0.5 && `GW${xp.gameWeek}: ${ (xp.totalXPoints).toFixed(2) } Pts`}</div>
                         </div>
                     ))}
                     </div>
