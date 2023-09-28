dd6dae997c98d3d7e71ef60959ea72484ca84928
Damar Krisnandi Ramdan <damarkrisnandi1202@gmail.com>
2023-09-28 20:45:17 +0700

    Update TabView.jsx

--- components/TabView.jsx
+++ components/TabView.jsx
@@ -4,13 +4,13 @@ export default function TabView(props) {
     const { defaultState, tabComponents } = props;
     const [tabState, setTabState] = useState(defaultState);
     return (
-        <div className="w-full bg-white rounded-lg shadow dark:bg-gray-200 p-4 pt-5 md:p-6 mb-2">
+        <div className="w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 pt-5 md:p-6 mb-2">
             <div className="flex justify-between mb-5">
-                <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+                <div className="text-sm font-medium text-center text-gray-900 border-b border-gray-200 dark:text-white dark:border-gray-700">
                     <ul className="flex flex-wrap -mb-px">
                         {tabComponents.map(tab => (
                             <li className="mr-2" key={tab.tabState}>
-                                <button className={`inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300
+                                <button className={`inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-blue-700 hover:border-blue-700 dark:hover:text-blue-700
                                 ${tab.tabState === tabState ? 'text-blue-700' : 'text-black'}
                                 `}
                                  onClick={() => {setTabState(tab.tabState)}}
